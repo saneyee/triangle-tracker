5ca44802f63278d274e7bc49190ba2aba6bb4412
Saneyee Sarkar and sravanthi velaga <sravanthi@>
2017-09-14 18:35:21 -0700

    Triangle tracker is done

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+$(function() {
+  $("form#formone").submit(function(event) {
+    $("#tri-Equi, #tri-Iso, #tri-Scal, #none").hide();
+    var sideA = parseInt($("input#side-a").val());
+    var sideB = parseInt($("input#side-b").val());
+    var sideC = parseInt($("input#side-c").val());
+
+        if (sideA, sideB, sideC) {
+        if (sideA >= sideB + sideC || sideB >= sideC + sideA || sideC >= sideB + sideA) {
+          $("#none").show();
+
+        }
+        else if (sideA === sideB && sideB === sideC && sideC === sideA) {
+          $("#tri-Equi").show();
+
+        }
+        else if (sideA === sideB || sideB === sideC || sideC === sideA) {
+          $("#tri-Iso").show();
+
+        }
+        else if (sideA != sideB && sideB != sideC && sideC != sideA) {
+          $("#tri-Scal").show();
+        }
+      }
+        else {
+          alert("Enter valid values");
+
+        }
+
+
+
+
+
+    event.preventDefault();
+
+});
+
+});
